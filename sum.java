4d98600e752b96ca051fd14a30f17a54251a8992
Yogendra pal <ypal75756@gmail.com>
2026-09-08 00:08:56 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,19 @@
+````
+import java.util.*;
+class sum
+{
+    public static void main(String[] args){
+
+    
+    int num1,num2,sum;
+   {
+   Scanner sc = new Scanner(System.in);
+    System.out.print("Enter N1: ");
+    num1=sc.nextInt();
+    System.out.print("Enter N2: ");
+    num2 = sc.nextInt();
+    sum = num1 + num2;
+    System.out.print("sum " + sum);
+    sc.close();
+      }   }
+}
